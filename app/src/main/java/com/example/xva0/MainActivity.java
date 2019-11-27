a0727d1aca1b79dbf5e94136f200fa51812ae8b3
JAVLONBEK <javlonsobirov36@gmail.com>
2019-11-27 22:35:58 +0500

    yuklab oldim

--- app/src/main/java/com/example/xva0/MainActivity.java
+++ app/src/main/java/com/example/xva0/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.xva0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
