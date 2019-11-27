8ca48e0fbcace3bd48086967726bf64dea5d3ee2
Sanat904327566 <Sanat.Ullixojayev904327566@gmail.com>
2019-11-27 22:44:03 +0500

    scscs dizayn yaratildi textview bitdi

--- app/src/main/java/com/example/xva0/MainActivity.java
+++ app/src/main/java/com/example/xva0/MainActivity.java
@@ -4,15 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CardView cardView;
+    private TextView textView1,textView2,textView3,textView4,textView5,textView6,textView7,textView8,textView9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textview2);
+        textView3 = findViewById(R.id.textview2);
+        textView4 = findViewById(R.id.textView3);
+        textView5 = findViewById(R.id.textView4);
+        textView6 = findViewById(R.id.textview5);
+        textView7 = findViewById(R.id.textview6);
+        textView8 = findViewById(R.id.textview7);
+        textView9 = findViewById(R.id.textView8);
     }
+
 }
